6b87b861fec4d7c4bd0ebcd3cbe3fe8772b22df9
Dmitriy Skudarnov <skrdvx@gmail.com>
2018-07-11 18:02:48 +0300

    Update header & footer styles
    Dmitriy

--- footer.php
+++ footer.php
@@ -2,16 +2,9 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h3 class="site-footer-title">
-          Get The Latest<span></span>
-        </h3>
+        <h3 class="site-footer-title">Get The Latest<span></span></h3>
       </div>
-      <div class="col-md-8">
-        Your Email
-      </div>
-      <div class="col-md-4">
-        Subscribe
-      </div>
+      <?php echo do_shortcode('[contact-form-7 id="200" title="Subscribe"]'); ?>
     </div>
     <div class="row">
       <div class="col-md-12">
@@ -19,58 +12,63 @@
           Follow LEAP<span></span>
         </h3>
         <div class="site-footer-socials">
-          Socials
+          <ul class="social-icons">
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+          </ul>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-3">
-        <h3 class="site-footer-subtitle">Why LEAP?</h3>
+      <div class="col-sm-6 col-md-3">
+        <h3 class="navbar-footer site-footer-subtitle">Why LEAP?</h3>
         <?php wp_nav_menu(
           array(
-            'depth'           => 2,
+            'depth'           => 1,
             'container'       => false,
             'theme_location'  => 'footer-1',
-            'menu_class' 	    => 'nav navbar-nav navbar-footer',
+            'menu_class' 	    => 'nav navbar-nav navbar-footer navbar-footer-1',
             'fallback_cb' 	  => 'wp_bootstrap_navwalker::fallback',
             'walker' 			    => new wp_bootstrap_navwalker()
           )
         ); ?>
       </div>
-      <div class="col-md-3">
-        <h3 class="site-footer-subtitle">Learn More</h3>
+      <div class="col-sm-6 col-md-3">
+        <h3 class="navbar-footer site-footer-subtitle">Learn More</h3>
         <?php wp_nav_menu(
           array(
-            'depth'           => 2,
+            'depth'           => 1,
             'container'       => false,
             'theme_location' 	=> 'footer-2',
-            'menu_class' 		  => 'nav navbar-nav navbar-footer',
+            'menu_class' 		  => 'nav navbar-nav navbar-footer navbar-footer-2',
             'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
             'walker' 			    => new wp_bootstrap_navwalker()
           )
         ); ?>
       </div>
-      <div class="col-md-3">
-        <h3 class="site-footer-subtitle">Connect</h3>
+      <div class="col-sm-6 col-md-3">
+        <h3 class="navbar-footer site-footer-subtitle">Connect</h3>
         <?php wp_nav_menu(
           array(
-            'depth'           => 2,
+            'depth'           => 1,
             'container'       => false,
             'theme_location' 	=> 'footer-3',
-            'menu_class' 		  => 'nav navbar-nav navbar-footer',
+            'menu_class' 		  => 'nav navbar-nav navbar-footer navbar-footer-3',
             'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
             'walker' 			    => new wp_bootstrap_navwalker()
           )
         ); ?>
       </div>
-      <div class="col-md-3">
-        <h3 class="site-footer-subtitle">For You</h3>
+      <div class="col-sm-6 col-md-3">
+        <h3 class="navbar-footer site-footer-subtitle">For You</h3>
         <?php wp_nav_menu(
           array(
-            'depth'           => 2,
+            'depth'           => 1,
             'container'       => false,
             'theme_location' 	=> 'footer-4',
-            'menu_class' 		  => 'nav navbar-nav navbar-footer',
+            'menu_class' 		  => 'nav navbar-nav navbar-footer navbar-footer-4',
             'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
             'walker' 			    => new wp_bootstrap_navwalker()
           )
--- header.php
+++ header.php
@@ -6,117 +6,56 @@
  *
  * @package _tk
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-	<?php do_action( 'before' ); ?>
-
-<?php // if (($post->ID == 116) or ($post->ID == 131)): ?>
-
-<?php /*
-<header class="siteHeader">
+<?php do_action( 'before' ); ?>
+<header class="site-navigation navbar navbar-expand-xl navbar-fixed-top">
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-			</div>
+		<div class="navbar-header">
+			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+				<div class="icon-group">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</div>
+				<span class="icon-text"><?php _e('Menu','_tk') ?> </span>
+			</button>
+			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<?php bloginfo( 'name' ); ?>
+			</a>
 		</div>
+		<nav class="collapse navbar-collapse" id="navbar-collapse">
+			<?php wp_nav_menu(
+				array(
+					'depth'           => 2,
+					'container'       => false,
+					'theme_location' 	=> 'main',
+					'menu_class' 			=> 'nav navbar-nav navbar-main',
+					'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+					'walker' 					=> new wp_bootstrap_navwalker()
+				)
+			); ?>
+			<?php wp_nav_menu(
+				array(
+					'depth'           => 2,
+					'container'       => false,
+					'theme_location' 	=> 'more',
+					'menu_class' 			=> 'nav navbar-nav navbar-more',
+					'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+					'walker' 					=> new wp_bootstrap_navwalker()
+				)
+			); ?>
+		</nav>
 	</div>
 </header>
-*/ ?>
-
-<?php // else: ?>
-
-<?php /*
-<header id="masthead" class="site-header" role="banner">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-header-inner col-sm-12">
-
-				<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-					</a>
-				<?php } // end if ( ! empty( $header_image ) ) ?>
-				<div class="site-branding">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description lead"><?php bloginfo( 'description' ); ?></p>
-				</div>
-			</div>
-		</div>
-	</div><!-- .container -->
-</header><!-- #masthead -->
-*/ ?>
-
-<div class="site-navigation">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
-				<div class="navbar">
-
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-							<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-							<?php bloginfo( 'name' ); ?>
-						</a>
-					</div>
-
-					<nav class="collapse navbar-collapse" id="navbar-collapse">
-						<?php wp_nav_menu(
-							array(
-								'depth'           => 2,
-								'theme_location' 	=> 'main',
-								'menu_class' 			=> 'nav navbar-nav navbar-header',
-								'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-								'walker' 					=> new wp_bootstrap_navwalker()
-							)
-						); ?>
-
-						<ul class="nav navbar-nav navbar-account pull-right">
-							<li class="menu-item">
-								<a href="/account/">Account</a>
-							</li>
-							<?php if ( is_user_logged_in() ) { ?>
-								<li class="menu-item">
-									<a href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a>
-								</li>
-							<?php } ?>
-						</ul>
-
-						<?php wp_nav_menu(
-							array(
-								'depth'           => 2,
-								'theme_location' 	=> 'more',
-								'menu_class' 			=> 'nav navbar-nav navbar-account pull-right',
-								'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-								'walker' 					=> new wp_bootstrap_navwalker()
-							)
-						); ?>
-					</nav>
-
-				</div><!-- .navbar -->
-			</div>
-		</div>
-	</div><!-- .container -->
-</div><!-- .site-navigation -->
-
-<?php // endif; ?>
