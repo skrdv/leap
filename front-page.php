4a0377b9fb73a2acb368dfb196612b89a84d92ba
Dmitriy Skudarnov <skrdvx@gmail.com>
2018-07-19 13:47:45 +0300

    Update Home page
    Nikolay

--- front-page.php
+++ front-page.php
@@ -41,38 +41,64 @@
 <section class="pageSection pageSection-12" >
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-8 col-sm-offset-1">
-				<h2 class="pageSection-title">Crafting Ideal Learning Experiences</h2>
-				<div class="pageSection-text">
-				<p>We’re defining, refining, distributing and measuring the most effective programs, tools and resources to support and empower educators with a mission to transform every teacher, every classroom and every student. Everywhere.</p>
-				</div>
-			</div><!-- column -->
-		</div><!-- row -->
+			<div class="col-sm-12">
+				<h2 class="pageSection-title"><?php the_field('section3_title'); ?></h2>
+				<h3 class="pageSection-subtitle"><?php the_field('section3_subtitle'); ?></h3>
+			</div><!-- col-sm -->
+		</div><!-- row  -->
+				
+		<div class="row sectionCards">
+					<?php while (have_rows('section3_cards')): the_row(); ?>
+						<div class="col-sm-3 sectionCard">
+								<div class="sectionCard-photo"></div>
+								<div class="sectionCard-content">
+									<h3 class="sectionCard-title"><?php the_sub_field('title'); ?></h3>
+									<div class="sectionCard-text"><?php the_sub_field('text'); ?></div>	
+								</div>				
+								<div class="sectionCard-button">
+									<a href="<?php the_sub_field('link'); ?>">Learn More</a>
+								</div>
+						</div>	
+					<?php endwhile; ?>
+		</div><!-- sectionCards -->	
+				
+		
 	</div><!-- container -->
 </section><!-- pageSection -->
-
-<section class="pageSection pageSection-13"  style="background-image: url(<?php the_field('page_banner_3'); ?>);">
-	<div class="container">
-		<div class="row flex">
-			<?php $counter = 1; ?>
-			<?php while ( $counter < 7): ?>
-			<div class="col-sm-4 mainCard">
-				<?php if (( $counter != 2) and ($counter !=3)): ?>
-				<div class="mainCard-inner">
-					<h3 class="mainCard-title">Get the LEAP Learning Framework</h3>
-					<div class="mainCard-text">Get the LEAP Learning Framework, including definitions and strategies for putting personalized learning into practice today.</div>
-					<button class="mainCard-button">Download the Framework</button>
-				</div>	
-			<?php endif; ?>
-			</div><!-- column -->
 	
-			<?php $counter++; ?>
-			<?php endwhile; ?>
+	
+
+<section class="pageSection pageSection-13">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="pageSection-title"><?php the_field('section4_title'); ?></h2>
+			</div>
+		</div><!-- row -->
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<?php $founder = get_field('founder_photo'); ?>
+				<?php $founder = $founder[sizes][medium]; ?>
+				<?php $team = get_field('founder_photo'); ?>
+				<?php $team = $team[sizes][medium]; ?>
+
+				<div class="blockCompany">
+					<div class="blockCompany-founder">
+						<div class="blockCompany-founder-photo" style="background-image:url(<?php echo  $founder; ?>);"></div>
+						<h3 class="blockCompany-founder-name"><?php the_field('founder-name'); ?></h3>
+						<h4 class="blockCompany-founder-position"><?php the_field('founder-caption'); ?></h4>
+					</div>
+					<div class="blockCompany-team"  style="background-image:url(<?php echo  $team; ?>);"></div>
+					<div class="blockCompany-button">
+						<a href=""></a>
+					</div>	
+				</div><!-- blockCompany -->	
+			</div>		
 		</div><!-- row -->
 	</div><!-- container -->
 </section><!-- pageSection -->
 
-
+<?php /*
 <section class="pageSection pageSection-14">
 	<div class="container">
 		<div class="row">
@@ -102,6 +128,8 @@
 		</div><!-- row -->
 	</div><!-- container -->
 </section><!-- pageSection -->
+
+*/ ?>
 
 </div>
 
