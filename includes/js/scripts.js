6f737bf4a104bcab14708afcb58dc84d39c67d10
Dmitriy Skudarnov <skrdvx@gmail.com>
2018-08-02 17:21:25 +0300

    Add Home hero background animation
    Dmitriy

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,47 +1,65 @@
 (function($){
 
-    // $('form input').change(function() {
-    //   $(this).closest('form').submit();
-    //   // console.log('submit on input change');
-    // });
-    //
-    // $('form select').change(function() {
-    //   $(this).closest('form').submit();
-    //   // console.log('submit on select change');
-    // });
-    //
-    // $('#filter').submit(function(){
-    //   var filter = $('#filter');
-    //   $.ajax({
-    //     url:filter.attr('action'),
-    //     data:filter.serialize(),
-    //     type:filter.attr('method'),
-    //     beforeSend:function(xhr){
-    //       // filter.find('button').text('Processing...');
-    //     },
-    //     success:function(data){
-    //       // filter.find('button').text('Apply filter');
-    //       $('#response').html(data);
-    //     }
-    //   });
-    //   return false;
-    // });
+  function showComments() {
+    var btn = $('#comments-toggle'),
+        section = $('#comments-section');
 
-    function showComments() {
-      var btn = $('#comments-toggle'),
-          section = $('#comments-section');
+    btn.click(function(e){
+      e.preventDefault();
+      console.log('click');
+      if (section.hasClass('is-active')) {
+          section.removeClass('is-active');
+      } else {
+        section.addClass('is-active');
+      }
+    });
+  }
 
-          btn.click(function(e){
-            e.preventDefault();
-            console.log('click');
-            if (section.hasClass('is-active')) {
-                section.removeClass('is-active');
-            } else {
-              section.addClass('is-active');
-            }
-          });
-    }
+  function homaGallery() {
+    var gallery = $('.mainBanner .gallery'),
+        item = gallery.find('.gallery-item'),
+        first = item.first();
+
+    first.addClass('is-active');
+
+
+    setInterval(function(){
+      active = gallery.find('.is-active');
+      next = active.next();
+
+
+      next.removeClass('is-hidden');
+      next.addClass('is-active');
+      
+      active.removeClass('is-active');
+      active.addClass('is-hidden');
+
+      if(!next.hasClass('is-active')){
+        first.addClass('is-active');
+        first.removeClass('is-hidden');
+      }
+
+    }, 6000)
+
+  }
+
+  $(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+
+    homaGallery();
 
     showComments();
+
+  });
+
+
+
+
 
 })(jQuery);
--- parts/main-banner.php
+++ parts/main-banner.php
@@ -1,21 +1,107 @@
-<section class="mainBanner" style="background-image: url(<?php the_field('main_banner_image'); ?>);">	
-	
+<?php
+
+$poster   = get_the_post_thumbnail_url($post->ID, 'poster');
+
+$images = get_field('section_gallery');
+$size = 'full';
+
+?>
+
+
+<style media="screen">
+
+	.mainBanner {
+		height: 827px;
+	}
+
+	.mainBanner-title {
+		position: absolute;
+    left: 0;
+    bottom: 0;
+    margin: 0;
+		width: 100%;
+    height: 120px;
+    line-height: 120px;
+		text-align: left;
+	}
+
+	.mainBanner-title .container{
+		border-top: 4px solid #00B3E6;
+	}
+
+	.gallery-item {
+		width: 100%;
+		height: 100%;
+		background-size: cover;
+		background-image: url(<?php echo $poster; ?>);
+		transform: scale(1.2);
+		transition: 3s;
+		opacity: 0;
+	}
+
+	.gallery-item.is-active {
+		display: block !important;
+		opacity: 1;
+	}
+
+	.gallery-item.is-hidden {
+		display: none !important;
+	}
+
+	.gallery-item.is-animated {
+	  width: 100%;
+	  margin: 0;
+	  -webkit-animation: slide 15s ease-out infinite;
+	}
+
+	@-webkit-keyframes slide {
+	    from { background-position: 0 0; }
+	    to { background-position: 0 -100px; }
+	}
+
+</style>
+
+<?php if( $images ): ?>
+	<section class="mainBanner">
+		<div class="gallery is-overlay">
+        <?php foreach( $images as $image ): ?>
+            <div class="gallery-item is-animated"
+								 style="background-image: url(<?php echo $image[url]; ?>);">
+            </div>
+        <?php endforeach; ?>
+    </div>
+		<h1 class="mainBanner-title">
+			<div class="container">
+					<?php the_title(); ?>
+			</div>
+		</h1>
+	</section>
+<?php endif; ?>
+
+
+
+
+
+	</div>
+
+
+<?php /*
 <div id="carousel-main" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators mainBanner-indicators">
-	<?php while ( have_rows('main_slider')): the_row(); ?>  
+	<?php while ( have_rows('main_slider')): the_row(); ?>
 	<?php if (get_row_index() == 1) { $class = 'active'; } else {$class=""; }  ?>
-	<?php $slide = get_row_index() -1; ?>  
+	<?php $slide = get_row_index() -1; ?>
     <li data-target="#carousel-main" data-slide-to="<?php echo $slide; ?>" class="<?php echo $class; ?>"></li>
-	<?php endwhile; ?>  	  
+	<?php endwhile; ?>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
-	<?php while ( have_rows('main_slider')): the_row(); ?>  
+	<?php while ( have_rows('main_slider')): the_row(); ?>
 	<?php if (get_row_index() == 1) { $class = 'active'; } else {$class=""; }  ?>
-	<?php $bg = get_sub_field('image'); $bg = $bg[sizes][large]; ?>  
-	  
+	<?php $bg = get_sub_field('image'); $bg = $bg[sizes][large]; ?>
+
     <div class="mainBanner-item item <?php echo $class; ?>" style="background-image: url(<?php echo $bg; ?>);">
 		<div class="mainBanner-cloack">
 			<div class="container">
@@ -24,13 +110,12 @@
 						<h2 class="mainBanner-title"><?php the_sub_field('title'); ?></h2>
 					</div>
 				</div><!-- row -->
-			</div><!-- container -->	
-		</div><!-- cloack -->	
+			</div><!-- container -->
+		</div><!-- cloack -->
     </div><!-- item -->
-	 <?php endwhile; ?> 
+	 <?php endwhile; ?>
   </div><!-- carousel-inner -->
 
-<?php /*
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -39,12 +124,10 @@
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-*/ ?>
+
 	<a class="mainBanner-scroll">
 		Scroll<br><img src="<?php echo get_template_directory_uri(); ?>/images/scroll-icon.svg">
-		
 	</a>
 </div>
-
-	
-</section><!-- mainBanner -->	
+*/ ?>
+</section><!-- mainBanner -->
